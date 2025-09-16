636696c7e357c51e3097d47e712ed4c70d581a46
Lu1702 <128778245+Lu1702@users.noreply.github.com>
2025-09-16 14:22:46 +0700

    TESTVUIVE

--- LyThuyet_LTWebNC/FirstProject/FirstProject/obj/Debug/net6.0/FirstProject.AssemblyInfo.cs
+++ LyThuyet_LTWebNC/FirstProject/FirstProject/obj/Debug/net6.0/FirstProject.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FirstProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8b51211ecd059379191b1c964d8da8ccfe898f8f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+eba36265fccf1f1a2032285ca3f3b77cd4711003")]
 [assembly: System.Reflection.AssemblyProductAttribute("FirstProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FirstProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
