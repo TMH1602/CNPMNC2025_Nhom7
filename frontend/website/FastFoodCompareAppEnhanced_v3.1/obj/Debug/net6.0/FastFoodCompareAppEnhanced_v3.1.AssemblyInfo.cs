e98b37219f3125a416731eb788f3ecf751102ef2
TMH1602 <minhhoangtran1602@gmail.com>
2025-11-11 10:59:29 +0700

    chinh_sua_login

--- frontend/website/FastFoodCompareAppEnhanced_v3.1/obj/Debug/net6.0/FastFoodCompareAppEnhanced_v3.1.AssemblyInfo.cs
+++ frontend/website/FastFoodCompareAppEnhanced_v3.1/obj/Debug/net6.0/FastFoodCompareAppEnhanced_v3.1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FastFoodCompareAppEnhanced_v3.1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+32e434e76159ba7482371315036f5f7ae95bd54e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0f76a5f65b3b1acd9b8a9ed70f7a58db1c2816ad")]
 [assembly: System.Reflection.AssemblyProductAttribute("FastFoodCompareAppEnhanced_v3.1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FastFoodCompareAppEnhanced_v3.1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
