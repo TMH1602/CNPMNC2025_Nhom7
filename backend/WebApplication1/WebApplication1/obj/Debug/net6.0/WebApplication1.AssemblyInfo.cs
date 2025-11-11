1afe89daf762f91d4c5b620bb911878353baf9e7
Lu1702 <128778245+Lu1702@users.noreply.github.com>
2025-11-12 02:40:03 +0700

    123123123123123123123

--- backend/WebApplication1/WebApplication1/obj/Debug/net6.0/WebApplication1.AssemblyInfo.cs
+++ backend/WebApplication1/WebApplication1/obj/Debug/net6.0/WebApplication1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6e513514a3d5164dc55ae6226b80543c4893a908")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f3339c8e4eec60dc9dac0abf08eb4ca757361a3f")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
