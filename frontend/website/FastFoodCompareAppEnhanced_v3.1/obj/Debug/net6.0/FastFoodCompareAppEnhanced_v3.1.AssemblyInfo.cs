f141279b52477929141fc59d6bb736a1a6168002
Lu1702 <128778245+Lu1702@users.noreply.github.com>
2025-12-14 22:06:39 +0700

    thaydoibackground

--- frontend/website/FastFoodCompareAppEnhanced_v3.1/obj/Debug/net6.0/FastFoodCompareAppEnhanced_v3.1.AssemblyInfo.cs
+++ frontend/website/FastFoodCompareAppEnhanced_v3.1/obj/Debug/net6.0/FastFoodCompareAppEnhanced_v3.1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FastFoodCompareAppEnhanced_v3.1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+459f0e2554d4a0e164391bce7391f752e46b86db")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+25db024fe3a016491623cf93c888522352aa6e63")]
 [assembly: System.Reflection.AssemblyProductAttribute("FastFoodCompareAppEnhanced_v3.1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FastFoodCompareAppEnhanced_v3.1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
